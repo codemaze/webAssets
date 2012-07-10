05188b23afc8ad0b60da5b48f2da8ffd38ef516b
noem <noem@codemaze.se>
2012-07-10 23:07:12 +0200

    option to specify config path

--- php/util/config.php
+++ php/util/config.php
@@ -1,10 +1,12 @@
 <?php
 	class config {
 		private $name;
+		private $path;
 		private $properties;
 		
-		public function config($name) {
+		public function config($name, $path = "./") {
 			$this->name($name);
+			$this->path($path);
 		}
 		
 		public function add($property, $value) {
@@ -27,23 +29,24 @@
 		}
 		
 		public function name($name = NULL) { return($this->name = (is_string($name)) ? $name : $this->name); }
+		public function path($path = NULL) { return($this->path = (is_string($path)) ? $path : $this->path); }
 		
 		public function read() {
 			$name = $this->name();
 			
-			@include($name.".php");
+			@include($this->path().$name.".php");
 			
 			$this->properties = (isset($$name)) ? $$name : array();
 		}
 		
 		public function save() {
-			file_put_contents($this->name().".php", "<?php\n\t// Generated using webAssets @ ".date("Y-m-d H:i:s")."\n\t\n");
+			file_put_contents($this->path().$this->name().".php", "<?php\n\t// Generated using webAssets @ ".date("Y-m-d H:i:s")."\n\t\n");
 			
-			foreach ($this->properties as $property => $value) file_put_contents($this->name().".php", "\t$".$this->name()."['".$property."']"." = \"".$value."\";\n", FILE_APPEND);
+			foreach ($this->properties as $property => $value) file_put_contents($this->path().$this->name().".php", "\t$".$this->name()."['".$property."']"." = \"".$value."\";\n", FILE_APPEND);
 			
-			file_put_contents($this->name().".php", "?>", FILE_APPEND);
+			file_put_contents($this->path().$this->name().".php", "?>", FILE_APPEND);
 		}
 		
-		public function saved() { return(file_exists($this->name().".php")); }
+		public function saved() { return(file_exists($this->path().$this->name().".php")); }
 	}
 ?>
---
+++ test/test.php
@@ -0,0 +1,9 @@
+<?php
+	// Generated using webAssets @ 2012-07-10 23:05:04
+	
+	$test['mysql.host'] = "localhost";
+	$test['mysql.port'] = "3306";
+	$test['mysql.uname'] = "root";
+	$test['mysql.pword'] = "1234";
+	$test['mysql.db'] = "test";
+?>
